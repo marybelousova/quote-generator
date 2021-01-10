4c2397c4bb4ac3c21df5679036182247eee602aa
Maria <marybelousova@yahoo.com>
2021-01-09 21:15:08 -0500

    feat: add new quote, loader and btns

---
+++ script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText= document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false; 
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+}
+
+function newQuote() {
+    showLoadingSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    if(!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if(quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+// Get Quote From API
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiUrl = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText; 
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuotes);
+twitterBtn.addEventListener('click', tweetQuote); 
+
+// On load
+getQuotes();
+
